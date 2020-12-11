56f1427387476816960ef72fe67f3aa0cd793ce0
Hank <ragreenburg@gmail.com>
2020-12-11 10:34:52 -0800

    vetted pdf to txt

--- oop_scraping.py
+++ oop_scraping.py
@@ -19,8 +19,12 @@ class pdf_comparing:
         "user-data-dir=C:\\Users\\Hank\\AppData\\Local\\Google\\Chrome\\User Data"
     )
 
+    prefs = {"download.default_directory": r"C:\Users\Hank\Documents\Random Python Scripts\pdf\pdf_web_scrapping\pdfs"}
+    
     options.add_argument("--no-sandbox")
 
+    options.add_experimental_option("prefs", prefs)
+    
     options.add_argument(
         "--log-level=3"
     )  # This should get rid of a depreciation error.
@@ -50,91 +54,97 @@ class pdf_comparing:
             self.driver.find_element_by_xpath(
                 f'//*[@id="content"]/div/div/main/div/div/div[1]/div/div/div/ul/li[{i}]'
             ).click()
-            time.sleep(.5)
+            time.sleep(0.5)
 
         # multi_stage
         for i in range(1, 6):
             self.driver.find_element_by_xpath(
                 f"/html/body/div[1]/div/div/main/div/div/div[2]/div/div/div/ul/li[{i}]"
             ).click()
-            time.sleep(.5)
+            time.sleep(0.5)
 
         # vertical air
         for i in range(1, 8):
             self.driver.find_element_by_xpath(
                 f"/html/body/div[1]/div/div/main/div/div/div[3]/div/div/div/ul/li[{i}]"
             ).click()
-            time.sleep(.5)
+            time.sleep(0.5)
 
         # low temp
         for i in range(1, 10):
             self.driver.find_element_by_xpath(
                 f"/html/body/div[1]/div/div/main/div/div/div[4]/div/div/div/ul/li[{i}]"
             ).click()
-            time.sleep(.5)
+            time.sleep(0.5)
 
         # tandem
         for i in range(1, 10):
             self.driver.find_element_by_xpath(
                 f"/html/body/div[1]/div/div/main/div/div/div[5]/div/div/div/ul/li[{i}]"
             ).click()
-            time.sleep(.5)
+            time.sleep(0.5)
 
         # modular
         for i in range(1, 5):
             self.driver.find_element_by_xpath(
                 f"/html/body/div[1]/div/div/main/div/div/div[6]/div/div/div/ul/li[{i}]"
             ).click()
-            time.sleep(.5)
+            time.sleep(0.5)
 
         # expansion module
         for i in range(1, 9):
             self.driver.find_element_by_xpath(
                 f"/html/body/div[1]/div/div/main/div/div/div[7]/div/div/div/ul/li[{i}]"
             ).click()
-            time.sleep(.5)
+            time.sleep(0.5)
 
         # fire and ice
         for i in range(1, 10):
             self.driver.find_element_by_xpath(
                 f"/html/body/div[1]/div/div/main/div/div/div[8]/div/div/div/ul/li[{i}]"
             ).click()
-            time.sleep(.5)
+            time.sleep(0.5)
 
         # heaters
         for i in range(1, 8):
             self.driver.find_element_by_xpath(
                 f"/html/body/div[1]/div/div/main/div/div/div[9]/div/div/div/ul/li[{i}]"
             ).click()
-            time.sleep(.5)
-            
+            time.sleep(0.5)
+
         # Gives a few extra seconds to help the download finish.
         time.sleep(5)
+        print("You have successfully downloaded the pdfs.")
         # this will close the browser
         self.driver.close()
 
     @property
     def open_pdf(self):
+        save_path = r"C:\Users\Hank\Documents\Random Python Scripts\pdf\pdf_web_scrapping\pdfs"
         lists = os.walk(save_path)
         for path, dir, filenames in lists:
             for filename in filenames:
                 doc = os.path.join(path, filename)
                 with pdp.open(doc) as pdf:
                     first_page = pdf.pages[0]
-                    print(first_page.extract_text(), file=open('output.txt', "a"))
+                    print(first_page.extract_text(), file=open("output.txt", "a", encoding="utf-8"))
+                # Might want to make this go beforehand so it'll break up the sections
+                with open("output.txt", 'a', encoding="utf-8") as text:
+                    text.write(f"{filename}\n")
+                print(f"{filename} finished.")
                 # if filename.endswith('.xlsx'):
-                    # doc = os.path.join(path, filename)
-                    # print(doc)
+                # doc = os.path.join(path, filename)
+                # print(doc)
 
     @property
     def compare():
-        #this also needs to be fixed.
+        # this also needs to be fixed.
         with open(path3) as f1:
             f1_text = f1.read()
-    
+
         with open(path2) as f2:
             f2_text = f2.read()
-    
+
         # Edit this fromfile and tofile to be more accurate. Should be a.txt and b.txt
         for line in difflib.unified_diff(
             f1_text, f2_text, fromfile="file1", tofile="file2", lineterm=""
@@ -148,5 +158,7 @@ class pdf_comparing:
         except:
             print("These files are the same")
 
+
 pc = pdf_comparing()
-pc.collection
+# pc.collection # This downloads all the pdf's from the website
+pc.open_pdf
